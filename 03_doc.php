ce35792d602cf83612c105ac3662b5318a33a937
wangyu <wy_c@gmail.com>
2018-04-20 14:48:45 +0800

    finish messaage

---
+++ 03_doc.php
@@ -0,0 +1,34 @@
+<!--
+	php的文档结构: html css js php
+-->
+<html>
+	<head>
+		<title></title>
+		<style>
+			div{
+				border:1px solid red;
+			}
+		</style>
+	</head>
+	<body>
+		<div>过分的话</div>
+		<div>
+			78*99-66/33=<?php echo 78*99-66/33;?>
+			<br/>
+			9*8=echo 9*8;
+		</div>
+	</body>
+</html>
+
+
+<?php
+
+//....接下来都是php代码的话  "?>"可以不写
+
+
+
+
+
+
+
+
